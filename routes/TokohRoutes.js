89b1c00d895cc402e3ed943b4397ff5e2b21a7fc
Veyzaputri <129712217+Veyzaputri@users.noreply.github.com>
2025-07-17 23:07:14 +0700

    Update TokohRoutes.js

--- routes/TokohRoutes.js
+++ routes/TokohRoutes.js
@@ -15,8 +15,8 @@ const upload = multer({ storage: storage });
 
 router.get("/tokoh", getTokoh);
 router.get("/tokoh/:id_tokoh",authMiddleware, getTokohById);
-router.post("/add-tokoh",authMiddleware, upload.single('gambar'), createTokoh);
-router.put("/tokoh/:id_tokoh",authMiddleware, upload.single("gambar"), updateTokoh);
+router.post("/add-tokoh",authMiddleware, upload.single('file'), createTokoh);
+router.put("/tokoh/:id_tokoh",authMiddleware, upload.single("file"), updateTokoh);
 router.delete("/tokoh/:id_tokoh",authMiddleware, deleteTokoh);
 
 export default router;
