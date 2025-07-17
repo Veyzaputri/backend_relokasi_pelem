45866d0880359134ad9f2f9a346cb847c72e25b0
Veyzaputri <129712217+Veyzaputri@users.noreply.github.com>
2025-07-17 23:05:46 +0700

    Update TokohRoutes.js

--- routes/TokohRoutes.js
+++ routes/TokohRoutes.js
@@ -1,16 +1,3 @@
-import multer from "multer";
-// Konfigurasi penyimpanan file
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'public/uploads/'); // Pastikan folder ini ada
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
-  }
-});
-
-const upload = multer({ storage: storage });
-
 import express from "express";
 import {
     getTokoh,
@@ -20,12 +7,16 @@ import {
     deleteTokoh
 } from "../controller/TokohController.js";
 import { authMiddleware } from "../middleware/AuthMiddleware.js";
+import multer from "multer";
 
 const router = express.Router();
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 router.get("/tokoh", getTokoh);
 router.get("/tokoh/:id_tokoh",authMiddleware, getTokohById);
-router.post("/add-tokoh",authMiddleware, upload.single('file'), createTokoh);
-router.put("/tokoh/:id_tokoh",authMiddleware, upload.single("file"), updateTokoh);
+router.post("/add-tokoh",authMiddleware, upload.single('gambar'), createTokoh);
+router.put("/tokoh/:id_tokoh",authMiddleware, upload.single("gambar"), updateTokoh);
 router.delete("/tokoh/:id_tokoh",authMiddleware, deleteTokoh);
 
 export default router;
