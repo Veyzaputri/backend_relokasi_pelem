2a29138500e1be5841408a571f7f9b77f57d098c
Veyzaputri <129712217+Veyzaputri@users.noreply.github.com>
2025-07-21 15:49:05 +0700

    Update AuthMiddleware.js

--- middleware/AuthMiddleware.js
+++ middleware/AuthMiddleware.js
@@ -1,6 +1,12 @@
+import jwt from "jsonwebtoken";
+
 export function authMiddleware(req, res, next) {
-  if (!req.session.userId) {
-    return res.status(401).json({ msg: "Belum login" });
-  }
-  next();
+  const token = req.cookies.token;
+  if (!token) return res.status(401).json({ msg: "Belum login" });
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) return res.status(403).json({ msg: "Token tidak valid" });
+    req.userId = decoded.userId;
+    next();
+  });
 }
